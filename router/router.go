fd9c2599c5c37ed5011f61f3c2795e9878e51c4c
Wiston Mwinzi <wiston.mwinzi@gmail.com>
2021-10-05 07:02:33 +0300

    updated router

--- router/router.go
+++ router/router.go
@@ -1,23 +1,15 @@
 package router
 
-//import (
-//	"wistonk-devops-test/middleware"
-//
-//	"github.com/gorilla/mux"
-//)
-//
-//// Router is exported and used in main.go
-//func Router() *mux.Router {
-//
-//	router := mux.NewRouter().StrictSlash(true)
-//
-//	router.HandleFunc("/", middleware.Home)
-//	router.HandleFunc("/configs", middleware.GetConfigs).Methods("GET")
-//	router.HandleFunc("/configs", middleware.PostConfigs).Methods("POST")
-//	router.HandleFunc("/configs/{name}", middleware.GetConfigsByName).Methods("GET")
-//	router.HandleFunc("/configs/{name}", middleware.UpdateConfigsByName).Methods("PATCH")
-//	router.HandleFunc("/configs/{name}", middleware.DeleteConfigsByName).Methods("DELETE")
-//	router.HandleFunc("/search", middleware.SearchMetadataByKeyValue).Methods("GET").Queries("metadata.monitoring.enabled", "{status}")
-//
-//	return router
-//}
+import (
+	"github.com/gorilla/mux"
+	"github.com/wistonk/go-k8s-app-demo/middleware"
+)
+
+// Router is exported and used in main.go
+func Router() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/tree", middleware.GetMyFavouriteTree).Methods("GET")
+
+	return router
+}
