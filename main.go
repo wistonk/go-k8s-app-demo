3b826cdcae934c5b9c4e47ddd9007231b8bf8cf3
Wiston Mwinzi <wiston.mwinzi@gmail.com>
2021-10-05 06:52:08 +0300

    Scaffolding the GO Application

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package go_k8s_app_demo
+package main
+
+//import (
+//	"log"
+//	"net/http"
+//	"os"
+//	"github.com/fatih/color"
+//)
+//
+//
+//func main() {
+//
+//	//Get the value of the SERVE_PORT environment variable
+//	addr := os.Getenv("SERVE_PORT")
+//
+//	r := router.Router()
+//
+//	color.Red("server is listening at: localhost" + addr)
+//	log.Fatal(http.ListenAndServe(addr, r))
+//}
---
+++ middleware/handlers.go
@@ -0,0 +1,22 @@
+package middleware
+
+//import (
+//	"encoding/json"
+//	"fmt"
+//	"github.com/fatih/color"
+//	"github.com/gorilla/mux"
+//	"github.com/wistonk/go-k8s-app-demo"
+//	"io/ioutil"
+//	"log"
+//	"net/http"
+//)
+//
+//
+//
+//func GetMyFavouriteTree(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("content-type", "application/json")
+//	w.WriteHeader(http.StatusOK)
+//	color.Blue("Invoke GetConfigs API")
+//
+//	json.NewEncoder(w).Encode(models.Configs)
+//}
