c3b47a8f2e5dc5752398f00cef7f5ea47ec25058
Wiston Mwinzi <wiston.mwinzi@gmail.com>
2021-10-05 07:02:03 +0300

    updated middlewar

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,22 +1,18 @@
 package middleware
 
-//import (
-//	"encoding/json"
-//	"fmt"
-//	"github.com/fatih/color"
-//	"github.com/gorilla/mux"
-//	"github.com/wistonk/go-k8s-app-demo"
-//	"io/ioutil"
-//	"log"
-//	"net/http"
-//)
-//
-//
-//
-//func GetMyFavouriteTree(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("content-type", "application/json")
-//	w.WriteHeader(http.StatusOK)
-//	color.Blue("Invoke GetConfigs API")
-//
-//	json.NewEncoder(w).Encode(models.Configs)
-//}
+import (
+	"encoding/json"
+	"github.com/fatih/color"
+	"github.com/wistonk/go-k8s-app-demo/models"
+	"net/http"
+)
+
+
+
+func GetMyFavouriteTree(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	color.Blue("Invoke GetConfigs API")
+
+	json.NewEncoder(w).Encode(models.Results)
+}
