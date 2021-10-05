12da7bfc68bf6948e7f55a77229d2e25e8ed1f0b
Wiston Mwinzi <wiston.mwinzi@gmail.com>
2021-10-05 07:03:07 +0300

    updated main.go

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
-//import (
-//	"log"
-//	"net/http"
-//	"os"
-//	"github.com/fatih/color"
-//)
-//
-//
+import (
+	"log"
+	"net/http"
+	"os"
+	"github.com/fatih/color"
+	"github.com/wistonk/go-k8s-app-demo/router"
+)
+
+
 func main() {
-//
-//	//Get the value of the SERVE_PORT environment variable
-//	addr := os.Getenv("SERVE_PORT")
-//
-//	r := router.Router()
-//
-//	color.Red("server is listening at: localhost" + addr)
-//	log.Fatal(http.ListenAndServe(addr, r))
+
+	//Get the value of the SERVE_PORT environment variable
+	addr := os.Getenv("SERVE_PORT")
+
+	r := router.Router()
+
+	color.Red("server is listening at: localhost" + addr)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
